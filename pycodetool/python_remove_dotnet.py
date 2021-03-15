fa3933e4de9fc7ae12210ec5b7864ed75ab999de
poikilos <7557867+poikilos@users.noreply.github.com>
2021-03-15 03:53:48 -0400

    Ignore the first arg correctly.

--- pycodetool/python_remove_dotnet.py
+++ pycodetool/python_remove_dotnet.py
@@ -14,21 +14,26 @@ import pct
 
 args = list()
 print("I am "+os.path.basename(__file__))
-for arg in sys.argv:
+for i in range(0, len(sys.argv)):
+    if i == 0:
+        print("  ignoring arg:" + sys.argv[i])
+        continue
+    arg = sys.argv[i]
     if arg != os.path.basename(__file__):
         args.append(arg)
         if len(args) == 1:
-            print("  input file: "+arg)
-    else:
-        print("  ignoring arg:"+arg)
+            print("  input file: " + arg)
 if len(args) >= 2:
-    print("  output file: "+arg[1])
+    print("  output file: " + args[1])
     if len(args) >= 3:
-        print("  identifier list output file: "+arg[2])
+        print("  identifier list output file: " + arg[2])
+    out_list_path = None
+    if len(args) >= 3:
+        out_list_path = os.path.realpath(args[2])
     parser = pct.PCTParser(args[0])
     parser.framework_to_standard_python(args[1])
-    if len(args) >= 3:
-        parser.save_identifier_lists(args[2])
+    if out_list_path is not None:
+        parser.save_identifier_lists(out_list_path)
 else:
     print("")
     print("")
