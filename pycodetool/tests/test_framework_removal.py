811dfbc92a3359462d5d93fe478626189a11598f
poikilos <7557867+poikilos@users.noreply.github.com>
2022-07-20 00:12:48 -0400

    Move parsing tests to test_parsing.

--- pycodetool/tests/test_framework_removal.py
+++ pycodetool/tests/test_framework_removal.py
@@ -9,19 +9,6 @@ from pycodetool import (
     set_verbosity,
 )
 
-from pycodetool.cc0code import (
-    optionalD,
-)
-
-from pycodetool.parsing import (
-    assertEqual,
-    quoted_slices,
-    get_quoted_slices_error,
-    which_slice,
-    END_BEFORE_QUOTE_ERR,
-    in_any_slice,
-)
-
 profile = None
 AppDatas = None
 
@@ -60,177 +47,23 @@ test_data_dir_i = len(try_dirs) - 1
 try_dirs.append(ba)
 
 
-from pycodetool.parsing import (
-    find_unquoted_not_commented_not_parenthetical
-)
-
 class TestFrameworkRemoval(TestCase):
-    def test_find_unquoted_not_commented_not_parenthetical(self):
-        echo0("* find_unquoted_not_commented_not_parenthetical...")
-        found = find_unquoted_not_commented_not_parenthetical(
-            "x = (i + a) + b # a",
-            "a",
-        )
-        assertEqual(found, -1)
 
-
-        good_i = 14
-        found = find_unquoted_not_commented_not_parenthetical(
-            "x = (i + a) + a # a",
-            #              ^ for the test, good_s must be at good_i
-            "a",
-        )
-        assertEqual(found, good_i)
-
-        good_i = 14
-        found = find_unquoted_not_commented_not_parenthetical(
-            "x = (i + a) + a # a",
-            #              ^ for the test, good_s must be at good_i
-            "a",
-            step=-1,  # Ensure a commented good_s is skipped by going in reverse
-        )
-        assertEqual(found, good_i)
-
-        set_verbosity(1)
-
-        good_i = 20
-        found = find_unquoted_not_commented_not_parenthetical(
-            "x = 'a' + (i + a) + a # a",
-            #                    ^ for the test, this index must match good_i
-            "a",
-        )
-        assertEqual(found, good_i, tbs=("find a non-quoted non-parenthetical"
-                                        " string after other matches that are"))
-
-        good_i = 20
-        good_s = "a"
-        test_s = 'x = "a" + (i + a) + a # a'
-        #                             ^ for the test, good_s must be at good_i
-        assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
-                    tbs="The test itself is wrong: good_s is not at good_i")
-        found = find_unquoted_not_commented_not_parenthetical(
-            test_s,
-            good_s,
-        )
-        assertEqual(
-            found,
-            good_i,
-            tbs="finding a non-quoted non-parenthetical string",
-        )
-
-        test_s = 'x = \\"a" + (i + a) + \"a\" # a'
-        good_i = -1
-        good_s = "a"
-        # assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
-        #             tbs="The test itself is wrong: good_s is not at good_i")
-        # ^ don't check since good_i is -1 (not found is correct)
-        found = find_unquoted_not_commented_not_parenthetical(
-            test_s,
-            good_s,
-        )
-        assertEqual(found, good_i, tbs=("should find nothing since a backslash"
-                                        " outside of quotes doesn't count"))
-
-        test_s = 'x = "\\"" + a + (i + a) + \"a\" # a'
-        good_i = 11
-        good_s = "a"
-        assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
-                    tbs="The test itself is wrong: good_s is not at good_i")
-        found = find_unquoted_not_commented_not_parenthetical(
-            test_s,
-            good_s,
-        )
-        assertEqual(found, good_i, tbs="finding the first escaped string")
-
-        test_s = 'x = \'\\\'\' + a + (i + a) + \"a\" # a'
-        good_i = 11
-        good_s = "a"
-        assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
-                    tbs="The test itself is wrong: good_s is not at good_i")
-        found = find_unquoted_not_commented_not_parenthetical(
-            test_s,
-            good_s,
-        )
-        assertEqual(found, good_i, tbs=("finding the first escaped string with"
-                                        " an escaped quote"))
-
-        test_s = 'x = \'\\\'a\' + (i + a) + \"a\" # a'
-        good_i = 17
-        good_s = "a"
-        assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
-                    tbs="The test itself is wrong: good_s is not at good_i")
-        found = find_unquoted_not_commented_not_parenthetical(
-            test_s,
-            good_s,
-            step=-1,
-        )
-        assertEqual(found, good_i, tbs="finding the last escaped string")
-
-        # intentionally provide bad syntax to ensure the escape character is
-        # ignored when not in quotes:
-        test_s = 'x = \\"a" + (i + a) + \\"a\" # a'
-        good_i = 6
-        good_s = "a"
-        assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
-                    tbs="The test itself is wrong: good_s is not at good_i")
-        found = find_unquoted_not_commented_not_parenthetical(
-            test_s,
-            good_s,
-            step=-1,
-        )
-        assertEqual(found, good_i, tbs=("finding the last escaped string after"
-                                        " another escaped string"))
-
-    def test_quoted_slices(self):
-        echo0("* test quoted_slices...")
-        # intentionally botch the results by starting after the opening quote:
-        test_s = 'x = "a" + (i + a) + "a" # "b"'
-        goodIs = [(6, 21), (22, 27)]
-        assertEqual(test_s[goodIs[0][0]:goodIs[0][1]], '" + (i + a) + "',
-                    tbs="The silent degradation test itself is wrong.")
-        assertEqual(test_s[goodIs[1][0]:goodIs[1][1]], '" # "',
-                    tbs="The silent degradation test itself is wrong.")
-        gotIs = quoted_slices(test_s, start=6)
-        self.assertAllEqual(goodIs, gotIs, tbs=("quoted slices {} should silently"
-                                           " degrade to {}"
-                                           "".format(gotIs, goodIs)))
-        assertEqual(get_quoted_slices_error(), END_BEFORE_QUOTE_ERR)
-        echo0("^ TEST: An unclosed quote is expected since start=6")
-
-        test_s = 'x = "a" + (i + a) + "a" # "c"'
-        goodIs = [(4, 7), (20, 23)]
-        assertEqual(test_s[goodIs[0][0]:goodIs[0][1]], '"a"',
-                    tbs="The test itself is wrong.")
-        assertEqual(test_s[goodIs[1][0]:goodIs[1][1]], '"a"',
-                    tbs="The test itself is wrong.")
-        gotIs = quoted_slices(test_s)
-        self.assertAllEqual(goodIs, gotIs, tbs=("quoted slices {} should be {}"
-                                           "".format(gotIs, goodIs)))
-        assertEqual(get_quoted_slices_error(), None)
-
-
-        goodIs = [(4, 6), (20, 22)]
-
-        w_slice = which_slice(21, goodIs)
-        assertEqual(w_slice, 1)
-        a_slice = in_any_slice(21, goodIs)
-        assertEqual(a_slice, True)
-
-        w_slice = which_slice(5, goodIs)
-        assertEqual(w_slice, 0)
-        a_slice = in_any_slice(5, goodIs)
-        assertEqual(a_slice, True)
-
-        w_slice = which_slice(6, goodIs)
-        assertEqual(w_slice, -1)
-        a_slice = in_any_slice(6, goodIs)
-        assertEqual(a_slice, False)
-
-
-        assertEqual(optionalD(11.123456, 5).format(11.123456), '11.12346')
-        assertEqual(optionalD(11.12345, 5).format(11.12345), '11.12345')
-        assertEqual(optionalD(11.1234, 5).format(11.1234), '11.1234')
-        assertEqual(optionalD(11, 5).format(11), '11')
+    def assertAllEqual(self, list1, list2, tbs=None):
+        '''
+        [copied from pycodetools.parsing by author]
+        '''
+        if len(list1) != len(list2):
+            echo0("The lists are not the same length: list1={}"
+                  " and list2={}".format(list1, list2))
+            self.assertEqual(len(list1), len(list2))
+        for i in range(len(list1)):
+            try:
+                self.assertEqual(list1[i], list2[i])
+            except AssertionError as ex:
+                if tbs is not None:
+                    echo0("reason string (tbs): " + tbs)
+                raise ex
 
     def test_framework_removal(self):
         echo0("* test framework to standard Python...")
@@ -266,18 +99,3 @@ class TestFrameworkRemoval(TestCase):
         echo0("  * saved identifiers to \"{}\"".format(id_outfile_path))
         echo0("  * saved result to \"{}\"".format(outfile_path))
 
-    def assertAllEqual(self, list1, list2, tbs=None):
-            '''
-            [copied from pycodetools.parsing by author]
-            '''
-            if len(list1) != len(list2):
-                echo0("The lists are not the same length: list1={}"
-                      " and list2={}".format(list1, list2))
-                self.assertEqual(len(list1), len(list2))
-            for i in range(len(list1)):
-                try:
-                    self.assertEqual(list1[i], list2[i])
-                except AssertionError as ex:
-                    if tbs is not None:
-                        echo0("reason string (tbs): " + tbs)
-                    raise ex
--- pycodetool/tests/test_parsing.py
+++ pycodetool/tests/test_parsing.py
@@ -19,10 +19,38 @@ from pycodetool import (
 
 from pycodetool.parsing import (
     quoted_slices,
+    get_quoted_slices_error,
+    which_slice,
+    in_any_slice,
+    END_BEFORE_QUOTE_ERR,
     explode_unquoted,
+    find_unquoted_not_commented_not_parenthetical,
+    assertEqual,  # This is a special one with custom output.
 )
 
+from pycodetool.cc0code import (
+    optionalD,
+)
+
+
 class TestParsing(unittest.TestCase):
+
+    def assertAllEqual(self, list1, list2, tbs=None):
+        '''
+        [copied from pycodetools.parsing by author]
+        '''
+        if len(list1) != len(list2):
+            echo0("The lists are not the same length: list1={}"
+                  " and list2={}".format(list1, list2))
+            self.assertEqual(len(list1), len(list2))
+        for i in range(len(list1)):
+            try:
+                self.assertEqual(list1[i], list2[i])
+            except AssertionError as ex:
+                if tbs is not None:
+                    echo0("reason string (tbs): " + tbs)
+                raise ex
+
     def test_quoted_slices(self):
         set_verbosity(1)
         subject = '"example\'s param1", \'"param 2"\', param3'
@@ -82,6 +110,58 @@ class TestParsing(unittest.TestCase):
             parts.append(subject[params[0]:params[1]])
         self.assertEqual(parts, ['"a"', '"a"'])
 
+
+    def test_quoted_slices_start(self):
+        echo0("* test quoted_slices...")
+        # intentionally botch the results by starting after the opening quote:
+        test_s = 'x = "a" + (i + a) + "a" # "b"'
+        goodIs = [(6, 21), (22, 27)]
+        assertEqual(test_s[goodIs[0][0]:goodIs[0][1]], '" + (i + a) + "',
+                    tbs="The silent degradation test itself is wrong.")
+        assertEqual(test_s[goodIs[1][0]:goodIs[1][1]], '" # "',
+                    tbs="The silent degradation test itself is wrong.")
+        gotIs = quoted_slices(test_s, start=6)
+        self.assertAllEqual(goodIs, gotIs, tbs=("quoted slices {} should silently"
+                                           " degrade to {}"
+                                           "".format(gotIs, goodIs)))
+        assertEqual(get_quoted_slices_error(), END_BEFORE_QUOTE_ERR)
+        echo0("^ TEST: An unclosed quote is expected since start=6")
+
+        test_s = 'x = "a" + (i + a) + "a" # "c"'
+        goodIs = [(4, 7), (20, 23)]
+        assertEqual(test_s[goodIs[0][0]:goodIs[0][1]], '"a"',
+                    tbs="The test itself is wrong.")
+        assertEqual(test_s[goodIs[1][0]:goodIs[1][1]], '"a"',
+                    tbs="The test itself is wrong.")
+        gotIs = quoted_slices(test_s)
+        self.assertAllEqual(goodIs, gotIs, tbs=("quoted slices {} should be {}"
+                                           "".format(gotIs, goodIs)))
+        assertEqual(get_quoted_slices_error(), None)
+
+
+        goodIs = [(4, 6), (20, 22)]
+
+        w_slice = which_slice(21, goodIs)
+        assertEqual(w_slice, 1)
+        a_slice = in_any_slice(21, goodIs)
+        assertEqual(a_slice, True)
+
+        w_slice = which_slice(5, goodIs)
+        assertEqual(w_slice, 0)
+        a_slice = in_any_slice(5, goodIs)
+        assertEqual(a_slice, True)
+
+        w_slice = which_slice(6, goodIs)
+        assertEqual(w_slice, -1)
+        a_slice = in_any_slice(6, goodIs)
+        assertEqual(a_slice, False)
+
+
+        assertEqual(optionalD(11.123456, 5).format(11.123456), '11.12346')
+        assertEqual(optionalD(11.12345, 5).format(11.12345), '11.12345')
+        assertEqual(optionalD(11.1234, 5).format(11.1234), '11.1234')
+        assertEqual(optionalD(11, 5).format(11), '11')
+
     def test_explode_strings(self):
         set_verbosity(1)
         subject = '"example\'s param1", \'"param 2"\', param3'
@@ -106,7 +186,6 @@ class TestParsing(unittest.TestCase):
             results,
             good_params
         )
-
 
     def test_explode_strings_and_indices(self):
         set_verbosity(1)
@@ -137,3 +216,118 @@ class TestParsing(unittest.TestCase):
             results,
             good_pairs
         )
+
+    def test_find_unquoted_not_commented_not_parenthetical(self):
+        echo0("* find_unquoted_not_commented_not_parenthetical...")
+        found = find_unquoted_not_commented_not_parenthetical(
+            "x = (i + a) + b # a",
+            "a",
+        )
+        assertEqual(found, -1)
+
+        good_i = 14
+        found = find_unquoted_not_commented_not_parenthetical(
+            "x = (i + a) + a # a",
+            #              ^ for the test, good_s must be at good_i
+            "a",
+        )
+        assertEqual(found, good_i)
+
+        good_i = 14
+        found = find_unquoted_not_commented_not_parenthetical(
+            "x = (i + a) + a # a",
+            #              ^ for the test, good_s must be at good_i
+            "a",
+            step=-1,  # Ensure a commented good_s is skipped by going in reverse
+        )
+        assertEqual(found, good_i)
+
+        set_verbosity(1)
+
+        good_i = 20
+        found = find_unquoted_not_commented_not_parenthetical(
+            "x = 'a' + (i + a) + a # a",
+            #                    ^ for the test, this index must match good_i
+            "a",
+        )
+        assertEqual(found, good_i, tbs=("find a non-quoted non-parenthetical"
+                                        " string after other matches that are"))
+
+        good_i = 20
+        good_s = "a"
+        test_s = 'x = "a" + (i + a) + a # a'
+        #                             ^ for the test, good_s must be at good_i
+        assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
+                    tbs="The test itself is wrong: good_s is not at good_i")
+        found = find_unquoted_not_commented_not_parenthetical(
+            test_s,
+            good_s,
+        )
+        assertEqual(
+            found,
+            good_i,
+            tbs="finding a non-quoted non-parenthetical string",
+        )
+
+        test_s = 'x = \\"a" + (i + a) + \"a\" # a'
+        good_i = -1
+        good_s = "a"
+        # assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
+        #             tbs="The test itself is wrong: good_s is not at good_i")
+        # ^ don't check since good_i is -1 (not found is correct)
+        found = find_unquoted_not_commented_not_parenthetical(
+            test_s,
+            good_s,
+        )
+        assertEqual(found, good_i, tbs=("should find nothing since a backslash"
+                                        " outside of quotes doesn't count"))
+
+        test_s = 'x = "\\"" + a + (i + a) + \"a\" # a'
+        good_i = 11
+        good_s = "a"
+        assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
+                    tbs="The test itself is wrong: good_s is not at good_i")
+        found = find_unquoted_not_commented_not_parenthetical(
+            test_s,
+            good_s,
+        )
+        assertEqual(found, good_i, tbs="finding the first escaped string")
+
+        test_s = 'x = \'\\\'\' + a + (i + a) + \"a\" # a'
+        good_i = 11
+        good_s = "a"
+        assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
+                    tbs="The test itself is wrong: good_s is not at good_i")
+        found = find_unquoted_not_commented_not_parenthetical(
+            test_s,
+            good_s,
+        )
+        assertEqual(found, good_i, tbs=("finding the first escaped string with"
+                                        " an escaped quote"))
+
+        test_s = 'x = \'\\\'a\' + (i + a) + \"a\" # a'
+        good_i = 17
+        good_s = "a"
+        assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
+                    tbs="The test itself is wrong: good_s is not at good_i")
+        found = find_unquoted_not_commented_not_parenthetical(
+            test_s,
+            good_s,
+            step=-1,
+        )
+        assertEqual(found, good_i, tbs="finding the last escaped string")
+
+        # intentionally provide bad syntax to ensure the escape character is
+        # ignored when not in quotes:
+        test_s = 'x = \\"a" + (i + a) + \\"a\" # a'
+        good_i = 6
+        good_s = "a"
+        assertEqual(good_s, test_s[good_i:good_i+len(good_s)],
+                    tbs="The test itself is wrong: good_s is not at good_i")
+        found = find_unquoted_not_commented_not_parenthetical(
+            test_s,
+            good_s,
+            step=-1,
+        )
+        assertEqual(found, good_i, tbs=("finding the last escaped string after"
+                                        " another escaped string"))
