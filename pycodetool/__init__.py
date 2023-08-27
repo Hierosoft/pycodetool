2fdd8b8c6ed89678c68a43521a98a30407eb8502
poikilos <7557867+poikilos@users.noreply.github.com>
2023-08-27 14:48:54 -0400

    Update imports for latest hierosoft.

--- pycodetool/__init__.py
+++ pycodetool/__init__.py
@@ -9,13 +9,16 @@ DATA_DIR = os.path.join(MODULE_DIR, "data")
 
 from .find_hierosoft import hierosoft
 
-from hierosoft.logging import (
+from hierosoft import (
     get_verbosity,
     set_verbosity,
     echo0,
     echo1,
     echo2,
     echo3,
+)
+
+from hierosoft.morelogging import (
     set_syntax_error_fmt,
     to_syntax_error,
     echo_SyntaxWarning,
